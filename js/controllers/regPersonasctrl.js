3acbd1d0e3f345e1cd68476ca7506efe841f9f98
Ald121 <fallsouls@hotmail.com>
2016-06-10 08:57:58 -0500

    registro empresas,personas

---
+++ js/controllers/regPersonasctrl.js
@@ -0,0 +1,27 @@
+ 
+ app.controller('PersonasCtrl', function ($scope,loaddatosSRI, Persona) {
+ 	 console.log('Personas');
+
+    $scope.cargadatos = function(estado) {
+        if (estado) {
+            loaddatosSRI.get({
+                nrodocumento: $("#cedula").val(),
+                tipodocumento: "CEDULA"
+            }).$promise.then(function(data) {
+                $scope.data.nombres_apellidos = data.nombres_apellidos;
+                $scope.data.provincia = data.provincia;
+                $scope.data.canton = data.canton;
+                $scope.data.parroquia = data.parroquia;
+                $scope.data.zona = data.zona;
+                console.log(data);
+            }, function(err) {
+                console.log(err.data.error);
+            });
+        } 
+    }
+    $scope.registrar = function() {
+        Persona.save($scope.data);
+        console.log($scope.data);
+    }
+
+    });

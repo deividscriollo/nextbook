f273d67996e3778da80bee4cd7a4fd6ad93c235a
Deivid <deividscriollo@gmail.com>
2016-06-13 18:17:50 -0500

    nex
    expaceo+

---
+++ js/controllers/maincontroller.js
@@ -0,0 +1,14 @@
+ app.controller('MainCtrl', function($scope, $routeSegment, loader) {
+
+    $scope.$routeSegment = $routeSegment;
+    $scope.loader = loader;
+
+    $scope.$on('routeSegmentChange', function() {
+        loader.show = false;
+    });
+});
+
+app.controller('Main-Ctrl-user-valid', function($scope, $routeSegment, loader) {
+
+    
+});

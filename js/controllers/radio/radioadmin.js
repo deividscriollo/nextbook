33085068c0fb88ea8914be83c054ba40f1678b92
Deivid <deividscriollo@gmail.com>
2016-07-09 13:19:04 -0500

    update card

--- js/controllers/radio/radioadmin.js
+++ js/controllers/radio/radioadmin.js
@@ -1,8 +1,58 @@
 var app = angular.module('app');
 app.controller('radioadminCtrl', function ($scope, $localStorage) {
-	console.log('test');
+	console.log('tesasdft');
 });
 
 app.controller('newCtrl', function ($scope, $localStorage) {
-	$scope.ejemplo = 'hola mundo';
+	$scope.appslist = 	[
+							{
+								title:'Empresa',
+								descripcion:'descripcion'
+							},
+        					{
+        						title:'Clientes',
+        						descripcion:'descripcion'
+        					},
+        					{
+        						title:'Programas',
+        						descripcion:'descripcion'
+        					},
+        					{
+        						title:'Vendedores',
+        						descripcion:'descripcion'
+        					},
+        					{
+        						title:'Fichas de Ingreso',
+        						descripcion:'descripcion'
+        					},
+        					{
+        						title:'Ficha Invitados',
+        						descripcion:'descripcion'
+        					},
+        					{
+        						title:'Ficha Programas',
+        						descripcion:'descripcion'
+        					},
+        					{
+        						title:'Contrato Selectivo',
+        						descripcion:'descripcion'
+        					},
+        					{
+        						title:'Contrato Rotativo',
+        						descripcion:'descripcion'
+        					},
+        					{
+        						title:'Facturacion',
+        						descripcion:'descripcion'
+        					},
+        					{
+        						title:'Rol de Pagos',
+        						descripcion:'descripcion'
+        					},
+        					{
+        						title:'Privilegios',
+        						descripcion:'descripcion'
+        					},
+
+						];
 });
--- js/controllers/radioadmin.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('app');
-app.controller('radioadminCtrl', function ($scope, $localStorage) {
-	console.log('test');
-});
-
-app.controller('newCtrl', function ($scope, $localStorage) {
-	$scope.ejemplo = 'hola mundo';
-});

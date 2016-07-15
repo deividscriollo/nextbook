98fe2c06d5c01ec09589ab4a8c5f69109fa311d8
Ald121 <fallsouls@hotmail.com>
2016-07-15 17:45:03 -0500

    ip

--- js/services/services.js
+++ js/services/services.js
@@ -22,9 +22,9 @@ app.service('servicios', function($resource, $localStorage, $location, ModalServ
 
                 // return "http://192.168.100.3/appnext/";
 
-                // return "http://192.168.100.17/appnext/";
+                return "http://192.168.100.17/appnext/";
 
-                return "http://192.168.100.3/appnext/";
+                // return "http://192.168.100.3/appnext/";
             }
         }
     };

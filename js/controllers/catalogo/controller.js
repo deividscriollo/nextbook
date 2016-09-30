b67283befcf02cdc5201ab7e50f9fa79057f781b
Oskrin <oskrs11@hotmail.com>
2016-09-30 15:51:18 -0500

    cambios

--- js/controllers/catalogo/controller.js
+++ js/controllers/catalogo/controller.js
@@ -1,4 +1,5 @@
 var app = angular.module('app');
-	app.controller('Catalogo-Inicio-Ctrl', function () {
-		console.log('test');
-	});
+
+app.controller('Catalogo-Inicio-Ctrl', function($mdDialog, $scope, serviciosfacturanext, servicios, $timeout, $localStorage) {
+	
+});

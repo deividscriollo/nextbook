a76b5c97c1a27241c589f75f01b29e9eb529273f
Oskrin <oskrs11@hotmail.com>
2016-09-30 17:17:46 -0500

    cambios

--- js/controllers/catalogo/controller.js
+++ js/controllers/catalogo/controller.js
@@ -1,9 +1,15 @@
 var app = angular.module('app');
 
 app.controller('Catalogo-Inicio-Ctrl', function($mdDialog, $scope, serviciosfacturanext, servicios, $timeout, $localStorage) {
-	$("#book").turn({
-		width: 400,
-		height: 300,
-		autoCenter: true
-	});
+	$('#magazine').turn({
+							display: 'double',
+							acceleration: true,
+							gradients: !$.isTouch,
+							elevation:50,
+							when: {
+								turned: function(e, page) {
+									/*console.log('Current view: ', $(this).turn('view'));*/
+								}
+							}
+						});
 });

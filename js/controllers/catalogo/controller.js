ea47e42b2cdc1b08f5d2b202e29403a03334292e
Oskrin <oskrs11@hotmail.com>
2016-09-30 16:29:50 -0500

    cambios

--- js/controllers/catalogo/controller.js
+++ js/controllers/catalogo/controller.js
@@ -1,5 +1,9 @@
 var app = angular.module('app');
 
 app.controller('Catalogo-Inicio-Ctrl', function($mdDialog, $scope, serviciosfacturanext, servicios, $timeout, $localStorage) {
-	
+	$("#book").turn({
+		width: 400,
+		height: 300,
+		autoCenter: true
+	});
 });

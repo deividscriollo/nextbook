918b2b1952811127fcfedd2b4c1370ede451158b
Oskrin <oskrs11@hotmail.com>
2016-10-04 09:00:41 -0500

    cambios

--- js/controllers/catalogo/controller.js
+++ js/controllers/catalogo/controller.js
@@ -1,15 +1,53 @@
 var app = angular.module('app');
 
 app.controller('Catalogo-Inicio-Ctrl', function($mdDialog, $scope, serviciosfacturanext, servicios, $timeout, $localStorage) {
-	$('#magazine').turn({
-							display: 'double',
-							acceleration: true,
-							gradients: !$.isTouch,
-							elevation:50,
-							when: {
-								turned: function(e, page) {
-									/*console.log('Current view: ', $(this).turn('view'));*/
-								}
-							}
-						});
+    $scope.imagePath = 'img/images/chompa.jpg';
+
+    // $('#magazine').turn({gradients: true, acceleration: true});
+    // $('#magazine').turn({
+    //                         display: 'double',
+    //                         acceleration: true,
+    //                         gradients: !$.isTouch,
+    //                         elevation:50,
+    //                         when: {
+    //                             turned: function(e, page) {
+    //                                 /*console.log('Current view: ', $(this).turn('view'));*/
+    //                             }
+    //                         }
+    //                     });
+
+    // $scope.book.turn({
+    //     acceleration : false,
+    //             display: 'double',
+    //             acceleration: true,
+    //             disable: false    
+    // })
+
+    // $('#book').turn({
+    //                         display: 'double',
+    //                         acceleration: false,
+    //                         gradients: !$.isTouch,
+    //                         // elevation:50,
+    //                         when: {
+    //                             turned: function(e, page) {
+    //                                 /*console.log('Current view: ', $(this).turn('view'));*/
+    //                             }
+    //                         }
+    //                     });
+
+
+
+
+    // $('#book').turn({});
+    
+    // $('#book').turn({
+    //             // acceleration : false,
+    //             // display: 'double',
+    //             // acceleration: true,
+    //             // disable: false
+                
+    //         });
+
+
+
 });

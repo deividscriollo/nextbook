5eefffd5e46b9f9fdceb1206a7be486da2e171c0
Oskrin <oskrs11@hotmail.com>
2016-10-05 09:06:14 -0500

    cambios

--- js/controllers/catalogo/controller.js
+++ js/controllers/catalogo/controller.js
@@ -1,53 +1,24 @@
 var app = angular.module('app');
 
 app.controller('Catalogo-Inicio-Ctrl', function($mdDialog, $scope, serviciosfacturanext, servicios, $timeout, $localStorage) {
-    $scope.imagePath = 'img/images/chompa.jpg';
-
-    // $('#magazine').turn({gradients: true, acceleration: true});
-    // $('#magazine').turn({
-    //                         display: 'double',
-    //                         acceleration: true,
-    //                         gradients: !$.isTouch,
-    //                         elevation:50,
-    //                         when: {
-    //                             turned: function(e, page) {
-    //                                 /*console.log('Current view: ', $(this).turn('view'));*/
-    //                             }
-    //                         }
-    //                     });
-
-    // $scope.book.turn({
-    //     acceleration : false,
-    //             display: 'double',
-    //             acceleration: true,
-    //             disable: false    
-    // })
-
-    // $('#book').turn({
-    //                         display: 'double',
-    //                         acceleration: false,
-    //                         gradients: !$.isTouch,
-    //                         // elevation:50,
-    //                         when: {
-    //                             turned: function(e, page) {
-    //                                 /*console.log('Current view: ', $(this).turn('view'));*/
-    //                             }
-    //                         }
-    //                     });
-
-
-
-
-    // $('#book').turn({});
-    
-    // $('#book').turn({
-    //             // acceleration : false,
-    //             // display: 'double',
-    //             // acceleration: true,
-    //             // disable: false
-                
-    //         });
-
-
+    return {
+	    restrict: 'E',
+	    link: function(scope, element, attrs) {
+	      $('#book').turn({
+	        width: '300px',
+	        height: '300px',
+	        pages: 8
+	      });
+	     
+	      
+	      $('#book').turn('peel', 'br');
+	    },
+	    controller: function($scope) {
+	      $scope.show_page = function(page) {
+	        console.log("page", page)
+	        $('#flipbook').turn('page', page);
+	      }
+	    },
+	  }
 
 });
